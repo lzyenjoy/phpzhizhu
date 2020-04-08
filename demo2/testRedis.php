cdbe16e07e7a63d3954799fcce78b2c4f13ed2b7
罗志禹公司的win7 <921243148@qq.com>
2020-04-08 16:46:50 +0800

    离职更新

--- demo2/testRedis.php
+++ demo2/testRedis.php
@@ -6,6 +6,7 @@
  * Time: 16:26
  */
 //连接本地的 Redis 服务
+header("Content-Type:text/html;charset=utf-8");
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
 echo "Connection to server sucessfully";
